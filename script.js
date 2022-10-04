89cc07e02630de14512f86559ed4036e1885e5a9
designhappily <65686852+shivakalai@users.noreply.github.com>
2022-10-04 12:11:25 +0200

    Form validation using javascript project started

---
+++ script.js
@@ -0,0 +1,11 @@
+let userInput = document.querySelector(".userInput");
+let emailInput = document.querySelector(".emailInput");
+let passwordInput = document.querySelector(".passwordInput");
+let confirmPasswordInput = document.querySelector(".confirmPasswordInput");
+let signBtn = document.querySelector(".signBtn");
+
+
+
+function Submit() {
+
+}
